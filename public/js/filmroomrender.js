aebbe0f000aa50d7be780134e33bfc014146c9a1
张乃赫 <zhangnaihe@example.com>
2019-05-15 20:35:49 +0800

    tiajiao

---
+++ public/js/filmroomrender.js
@@ -0,0 +1,146 @@
+let filmRoomShow = function () {
+    console.log(1);
+    $.ajax({
+        type: 'get',
+        url: '/filmRoomShow',
+        data: '',
+        success:function (data) {
+            // console.log(1)
+            let str = template('film-template',{list:data});
+
+            // console.log(str);
+            $('.e').html(str);
+        }
+    })
+}
+
+filmRoomShow();
+
+let s = function () {
+    $.ajax({
+        type: 'get',
+        url: '/filmRoomBrand',
+        data: '',
+        success: function (data) {
+            // console.log(data);
+        let str = template('brandroom_template',{list:data});
+        // console.log(str);
+        $('.a').html(str);
+        }
+    })
+}
+
+s();
+
+// 点击品牌进行查询
+$('.a').on('click','.btn-type',function () {
+    let id = $(this).data('id');
+
+    $.ajax({
+        type: 'get',
+        url: '/filmRoomPP',
+        data: { id: id },
+        success:function (data) {
+             if (data.code == '1010') {
+                 $('.e').html('<div>' + data.msg + '</div>')     
+             } else {
+                console.log(data);
+                let str = template('film-template',{list:data});
+                $('.e').html(str);
+             }
+            console.log(data)
+        }
+    })
+})
+// console.log('0');
+
+
+// 行政区
+let z = function () {
+    console.log(1);
+    $.ajax({
+      type: 'get',
+      url: '/filmRoomDistrict',
+      data: '',
+      success: function (data) {
+        console.log(data);
+        let str = template('districtroom_template', { list: data });
+
+        // console.log(str);
+        $('.b').html(str);
+      }
+    })
+  }
+  z();
+
+
+  // 点击品牌进行查询
+$('.a').on('click','.btn-type',function () {
+  let id = $(this).data('id');
+
+  $.ajax({
+      type: 'get',
+      url: '/filmRoomPP',
+      data: { id: id },
+      success:function (data) {
+           if (data.code == '1010') {
+               $('.e').html('<div>' + data.msg + '</div>')     
+           } else {
+              console.log(data);
+              let str = template('film-template',{list:data});
+              $('.e').html(str);
+           }
+          console.log(data)
+      }
+  })
+})
+  
+
+
+//   特殊厅
+let y = function () {
+    console.log(1);
+    $.ajax({
+      type: 'get',
+      url: '/filmRoomSpecial',
+      data: '',
+      success: function (data) {
+        console.log(data);
+        let str = template('specialroom_template', { list: data });
+
+        // console.log(str);
+        $('.c').html(str);
+      }
+    })
+  }
+  y();
+
+
+
+//   $('.btn-t').on('click', '.btn-type', function () {
+//     let id = $(this).data('id');
+//     console.log(id);
+//     $.ajax({
+//       type: 'get',
+//       url: '/filmAboutQy',
+//       data: { id: id },
+//       success: function (data) {
+//         if (data.code == '1002') {
+//           $('.movie-list').html('<div>'+data.msg+'</div>');
+//           console.log(data);
+//         } else {
+//         let str = template('filmabout-template', { list: data });
+//         $('.movie-list').html(str);
+//         }
+//         // console.log(data);
+     
+//       }
+//     })
+//   })
+
+
+
+
+
+
+
