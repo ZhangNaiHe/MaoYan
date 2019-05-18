45e39f586677275993177774d83e816db460d206
zhangjian <zhangjian@qq.com>
2019-05-18 09:20:40 +0800

    ct

--- routers/ticketRoute.js
+++ routers/ticketRoute.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const ticketsCtrl = require('../controller/ticketsCtrl');
-// 第一页
+// 显示路由
 router.get('/ticketselection-ZJ', ticketsCtrl.ticketShow);
 
 router.get('/ticketmovie',ticketsCtrl.ticketmovie);
 
 
 // router.get('/sqldata',ticketsCtrl.sqldata);
-
+// 提交路由
 router.get('/tickets', ticketsCtrl.tickets);
 module.exports = router;
